0064241f18def9defcc0e851ccecb3c2b8611a74
a22isahe <a22isahe@student.his.se>
2023-04-19 16:30:05 +0200

    Added functionality to go back to the previous screen to the button in activity2. added texinput field in activity1 for future use

--- app/src/main/java/com/example/screens/MainActivity2.java
+++ app/src/main/java/com/example/screens/MainActivity2.java
@@ -2,7 +2,10 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -12,12 +15,20 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         TextView text = (TextView) findViewById(R.id.Text1);
-
+        Button button2 = (Button) findViewById(R.id.button);
 
         Bundle extras = getIntent().getExtras();
         if(extras!= null) {
             String displayText = extras.getString("Text");
             text.setText(displayText);
         }
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
